76fdec7908053010837ab6187d9368b45812274b
DanKalh <92654593+DanKalh@users.noreply.github.com>
2024-06-28 20:56:22 -0500

    Update index.js (salma)

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
-// pages/index.js
+// src/pages/index.js
 import React from 'react';
 import Link from 'next/link';
+import styles from '../styles/HomePage.module.css'; // Import CSS module
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to the Volunteer Management System</h1>
-      <Link href="/login">Login</Link>
-      <br />
-      <Link href="/register">Register</Link>
+    <div className={styles.container}>
+      <nav className={styles.nav}>
+        <Link href="/login">Login</Link>
+        <Link href="/register">Register</Link>
+        <Link href="/profile">Profile</Link>
+        <Link href="/event-management">Event Management</Link>
+      </nav>
+      <div className={styles.content}>
+        <h1 className={styles.heading}>Welcome to the Volunteer Management System</h1>
+      </div>
     </div>
   );
 };
